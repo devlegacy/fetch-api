6c5e9f148c3677b32b6c50bd812f070ae3cc0618
Samuel R <samuel_-_rojas@hotmail.com>
2020-07-29 16:00:01 -0500

    Fetch: FormData - Upload files

--- src/index.ts
+++ src/index.ts
@@ -67,3 +67,26 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
 })
   .then((response: Response) => response.json()) // response: Uint8Array
   .then((post: Post) => console.log(post));
+
+document
+  .querySelector('#frm-upload')
+  ?.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    const target: HTMLFormElement = <HTMLFormElement>e.target;
+
+    const body = new FormData(target);
+    // const file = (<HTMLInputElement>document.querySelector('#file')).files;
+    // if (file) {
+    //   body.append('file', file[0]);
+    // }
+
+    fetch(target.action, {
+      method: 'POST',
+      body,
+    })
+      .then((response: Response) => response.text())
+      .then((data: string) => alert(data))
+      .catch((err: Error) =>
+        console.error('[Upload fetch error]:', err.message)
+      );
+  });
