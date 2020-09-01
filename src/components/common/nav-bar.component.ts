98aecc26806ba4e7ae369ecdb9317e9e730f3b53
Samuel R <samuel_-_rojas@hotmail.com>
2020-09-01 18:14:15 -0500

    Improve nav-bar.component

--- src/components/common/nav-bar.component.ts
+++ src/components/common/nav-bar.component.ts
@@ -1,25 +1,27 @@
+const DEFAULT_TARGET = 'navMenu';
+const NAV_TOGGLE_CLASS = 'is-active';
 export class NavBarComponent {
   constructor() {
     // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(
+    const $navbarBurger = Array.prototype.slice.call(
       document.querySelectorAll('.navbar-burger'),
       0
     );
 
     // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-      // Add a click event on each of them
-      $navbarBurgers.forEach((el) => {
-        el.addEventListener('click', () => {
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
+    // if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurger?.forEach(($nav: HTMLElement) => {
+      $nav.addEventListener('click', () => {
+        // Get the target from the "data-target" attribute
+        const target = $nav.dataset.target || DEFAULT_TARGET;
+        const $target = document.getElementById(target);
 
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target?.classList.toggle('is-active');
-        });
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $nav.classList.toggle(NAV_TOGGLE_CLASS);
+        $target?.classList.toggle(NAV_TOGGLE_CLASS);
       });
-    }
+    });
+    // }
   }
 }
