8965f47e35279318fbb7771bc09da4799fc63b18
Samuel R <samuel_-_rojas@hotmail.com>
2020-09-01 18:32:51 -0500

    Improve nav-bar.component, base-component

--- src/components/common/nav-bar.component.ts
+++ src/components/common/nav-bar.component.ts
@@ -1,27 +1,44 @@
+import { BaseComponent } from '../../core/components/base-component';
+import { ComponentOption } from '../../core/options/component-option';
+import { RenderedComponent } from '../../core/components/rendered-component';
+
 const DEFAULT_TARGET = 'navMenu';
 const NAV_TOGGLE_CLASS = 'is-active';
-export class NavBarComponent {
-  constructor() {
-    // Get all "navbar-burger" elements
-    const $navbarBurger = Array.prototype.slice.call(
-      document.querySelectorAll('.navbar-burger'),
+
+export class NavBarComponent
+  extends BaseComponent
+  implements RenderedComponent {
+  constructor(options: ComponentOption) {
+    super(options);
+  }
+
+  private findNavElements(): Array<HTMLElement> {
+    return Array.prototype.slice.call(
+      document.querySelectorAll(this.selector),
       0
     );
+  }
+
+  private handlerNavClick($nav: HTMLElement) {
+    $nav.addEventListener('click', () => {
+      // Get the target from the "data-target" attribute
+      const target = $nav.dataset.target || DEFAULT_TARGET;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $nav.classList.toggle(NAV_TOGGLE_CLASS);
+      $target?.classList.toggle(NAV_TOGGLE_CLASS);
+    });
+  }
+
+  render(): void {
+    // Get all "navbar-burger" elements
+    const $navbarBurger = this.findNavElements();
 
     // Check if there are any navbar burgers
     // if ($navbarBurgers.length > 0) {
     // Add a click event on each of them
-    $navbarBurger?.forEach(($nav: HTMLElement) => {
-      $nav.addEventListener('click', () => {
-        // Get the target from the "data-target" attribute
-        const target = $nav.dataset.target || DEFAULT_TARGET;
-        const $target = document.getElementById(target);
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $nav.classList.toggle(NAV_TOGGLE_CLASS);
-        $target?.classList.toggle(NAV_TOGGLE_CLASS);
-      });
-    });
+    $navbarBurger?.forEach(this.handlerNavClick);
     // }
   }
 }
--- src/core/components/base-component.ts
+++ src/core/components/base-component.ts
@@ -1,7 +1,9 @@
-export class BaseComponent {
-  public selector: string;
+import { ComponentOption } from '../options/component-option';
 
-  constructor(options: any) {
+export class BaseComponent {
+  protected selector: string;
+
+  constructor(options: ComponentOption) {
     this.selector = options.selector;
   }
 
@@ -12,7 +14,7 @@ export class BaseComponent {
       // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-7.html#optional-chaining
       // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator
       // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-9.html#parsing-differences-in-optional-chaining-and-non-null-assertions
-      $element!.innerHTML = options.data;
+      $element!.innerHTML = options?.data;
       // }
     }
   }
