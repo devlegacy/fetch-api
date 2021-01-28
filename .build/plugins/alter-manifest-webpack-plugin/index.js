59ad7a58acb9d67418f4f91995b7e07bb52a9dec
Samuel R <samuel_-_rojas@hotmail.com>
2021-01-28 03:18:34 -0500

    Update dependencies, add workbox dependencie, configure tsconfig, configure webpack for builds

---
+++ .build/plugins/alter-manifest-webpack-plugin/index.js
@@ -0,0 +1,82 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+class AlterManifestWebpackPlugin {
+  constructor(manifest) {
+    this.manifest = manifest;
+  }
+
+  apply(compiler) {
+    compiler.hooks.compilation.tap(
+      'AlterManifestWebpackPlugin',
+      (compilation) => {
+        HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+          'AlterManifestWebpackPlugin',
+          (data, cb) => {
+            if (data.html) {
+              data.html = data.html.split('>');
+              data.html = data.html
+                .filter((html) => html.length > 0)
+                .map((html) => {
+                  // Repair split
+                  html += '>';
+                  // Alter meta theme color, change from background color to theme color
+                  html =
+                    html ==
+                    `<meta name="theme-color" content="${this.manifest.background}">`
+                      ? `<meta name="theme-color" content="${this.manifest.theme_color}">`
+                      : html;
+                  // Alter meta msapplication tile color, change from background color to theme color
+                  html =
+                    html ==
+                    `<meta name="msapplication-TileColor" content="${this.manifest.background}">`
+                      ? `<meta name="msapplication-TileColor" content="${this.manifest.theme_color}">`
+                      : html;
+                  // Alter manifest, from manifest.json to manifest.webmanifest, according to example in: https://www.w3.org/TR/appmanifest/#using-a-link-element-to-link-to-a-manifest
+                  html = /(rel\=.{1}manifest.{1}.*)/.test(html)
+                    ? html.replace('manifest.json', 'manifest.webmanifest')
+                    : html;
+                  return html;
+                })
+                .reduce((prev, next) => {
+                  // Convert to string
+                  return `${prev}${next}`;
+                });
+            }
+
+            cb(null, data);
+          }
+        );
+      }
+    );
+
+    // compiler.hooks.make.tapAsync(
+    //   'AlterManifestWebpackPlugin',
+    //   (compilation, callback) => {
+    //     console.log(compilation.hooks.faviconsWebpackPlugin);
+    //     console.log(compilation.hooks.faviconsWebpackPluginMake);
+    //     console.log(compilation.hooks.faviconsWebpackPluginCompile);
+    //     console.log(compilation.hooks.faviconsWebpackPluginEmit);
+    //     console.log(compilation.hooks.faviconsWebpackPluginHtmlPluginBefore);
+
+    //     console.log(compilation.hooks.FaviconsWebpackPlugin);
+    //     console.log(compilation.hooks.FaviconsWebpackPluginMake);
+    //     console.log(compilation.hooks.FaviconsWebpackPluginCompile);
+    //     console.log(compilation.hooks.FaviconsWebpackPluginEmit);
+    //     console.log(compilation.hooks.FaviconsWebpackPluginHtmlPluginBefore);
+    //     // compilation.hooks.webappWebpackPluginBeforeEmit.tapAsync(
+    //     //   'B',
+    //     //   (result, callback) => {
+    //     //     // The result of favicons library can be modified here
+    //     //     // and it will be returned to WebApp Plugin to be emitted.
+    //     //     // Add your custom functions below
+    //     //     console.log(result);
+    //     //     // Return the custom result
+    //     //     return callback(null, result);
+    //     //   }
+    //     // );
+    //     return callback();
+    //   }
+    // );
+  }
+}
+module.exports = AlterManifestWebpackPlugin;
