db057e5b9b5ef2a96e947cb2bcb443f0856efc05
Samuel R <samuel_-_rojas@hotmail.com>
2021-01-28 05:13:25 -0500

    Configure templates and service worker install

--- src/assets/ts/index.ts
+++ src/assets/ts/index.ts
@@ -15,6 +15,21 @@ class App {
       new ToDoCardComponent({ selector: '#to-do-container' }).render();
       new CreateToDoComponent({ selector: '#frm-create-post' }).render();
     });
+
+    if ('serviceWorker' in navigator) {
+      // Register a service worker hosted at the root of the
+      // site using the default scope.
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then(function (registration) {
+          console.log('Service worker registration succeeded:', registration);
+        })
+        .catch(function (error) {
+          console.log('Service worker registration failed:', error);
+        });
+    } else {
+      console.log('Service workers are not supported.');
+    }
   }
 }
 
--- src/assets/ts/sw.ts
+++ src/assets/ts/sw.ts
@@ -1,4 +1,49 @@
 import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
 
 // @ts-ignore: __WB_MANIFEST is a placeholder filled by workbox-webpack-plugin with the list of dependencies to be cached
 precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  // (routeData) => {
+  // return routeData.event.request.headers.get('accept').includes('text/html');
+  new RegExp('/.*\\.html$'),
+  // }
+
+  async ({ url, request, event, params }) => {
+    const response = await fetch(request);
+    const responseBody = await response.text();
+    return new Response(`${responseBody} <!-- Look Ma. Added Content. -->`, {
+      headers: response.headers,
+    });
+    // const e = event;
+    // return caches
+    //   .match(e.request)
+    //   .then((response) => {
+    //     return (
+    //       response ||
+    //       fetch(e.request)
+    //         .then((res) => {
+    //           const cacheCopy = res.clone();
+    //           caches
+    //             .open('dynamic')
+    //             .then((cache) => {
+    //               cache.put(e.request.url, cacheCopy);
+    //               return res;
+    //             })
+    //             .catch((err) =>
+    //               console.error('[sw]:', 'Falló apertura de cache', err)
+    //             );
+    //           return res;
+    //         })
+    //         .catch((err) => {
+    //           console.error('[sw]: ', `Fetch, ${e.request.url}`, err);
+    //           return caches.match(`/index.html`).then((response) => {
+    //             return response;
+    //           });
+    //         })
+    //     );
+    //   })
+    //   .catch((err) => console.error('[sw]:', 'Fetch, falló respondWith', err));
+  }
+);
