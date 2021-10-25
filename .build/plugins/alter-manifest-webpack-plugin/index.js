32b6bcae629b39554bb50f20d5671be2353b1e91
Samuel R <samuel_-_rojas@hotmail.com>
2021-10-24 20:58:06 -0500

    chore: update packages - alter manifest

--- .build/plugins/alter-manifest-webpack-plugin/index.js
+++ .build/plugins/alter-manifest-webpack-plugin/index.js
@@ -5,84 +5,45 @@ class AlterManifestWebpackPlugin {
     this.manifest = manifest;
   }
 
+  /**
+   * @param {import('webpack').Compiler} compiler
+   */
   apply(compiler) {
-    compiler.hooks.compilation.tap('AlterManifestWebpackPlugin', (compilation) => {
-      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync('AlterManifestWebpackPlugin', (data, cb) => {
-        if (data.html) {
-          // console.log(data.html.includes(`<meta name="theme-color" content="${this.manifest.background}">`));
-          // console.log(
-          //   data.html.includes(`<meta name="msapplication-TileColor" content="${this.manifest.background}">`)
-          // );
-          // console.log(data.html);
-          data.html = data.html
-            .replace(
-              `<meta name="theme-color" content="${this.manifest.background}">`,
-              `<meta name="theme-color" content="${this.manifest.theme_color}">`
-            )
-            .replace(
-              `<meta name="msapplication-TileColor" content="${this.manifest.background}">`,
-              `<meta name="msapplication-TileColor" content="${this.manifest.theme_color}">`
-            );
-          // .split('>')
-          // .filter((html) => html.length > 0)
-          // .map((html) => {
-          //   console.log(html);
-          //   // Repair split
-          //   html += '>';
-          //   // Alter meta theme color, change from background color to theme color
-          //   html =
-          //     html == `<meta name="theme-color" content="${this.manifest.background}">`
-          //       ? `<meta name="theme-color" content="${this.manifest.theme_color}">`
-          //       : html;
-          //   // Alter meta msapplication tile color, change from background color to theme color
-          //   html =
-          //     html == `<meta name="msapplication-TileColor" content="${this.manifest.background}">`
-          //       ? `<meta name="msapplication-TileColor" content="${this.manifest.theme_color}">`
-          //       : html;
-          //   // Alter manifest, from manifest.json to manifest.webmanifest, according to example in: https://www.w3.org/TR/appmanifest/#using-a-link-element-to-link-to-a-manifest
-          //   // html = /(rel\=.{1}manifest.{1}.*)/.test(html)
-          //   //   ? html.replace('manifest.json', 'manifest.webmanifest')
-          //   //   : html;
-          //   return html;
-          // })
-          // .reduce((prev, next) => {
-          //   // Convert to string
-          //   return `${prev}${next}`;
-          // });
-        }
+    const webpack = compiler.webpack;
+    const Compilation = webpack.Compilation;
 
-        cb(null, data);
+    const publicPath = compiler.options.output.publicPath;
+
+    compiler.hooks.initialize.tap('AlterManifestWebpackPlugin', () => {
+      compiler.hooks.thisCompilation.tap('AlterManifestWebpackPlugin', (compilation) => {
+        compilation.hooks.processAssets.tap(
+          {
+            name: 'AlterManifestWebpackPlugin',
+            stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS, // see below for more stages
+          },
+          (assets) => {
+            if (assets['manifest.json'] && !assets['manifest.webmanifest']) {
+              assets['manifest.webmanifest'] = assets['manifest.json'];
+            }
+          }
+        );
+      });
+      compiler.hooks.compilation.tap('AlterManifestWebpackPlugin', (compilation) => {
+        // Static Plugin interface |compilation |HOOK NAME | register listener
+        HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+          'AlterManifestWebpackPlugin', // <-- Set a meaningful name here for stacktraces
+          (data, cb) => {
+            // Manipulate the content
+            data.html = data.html.replace(
+              '<link rel="manifest"',
+              `<link rel="manifest" href="${publicPath}manifest.webmanifest"><link rel="manifest"`
+            );
+            // Tell webpack to move on
+            cb(null, data);
+          }
+        );
       });
     });
-
-    // compiler.hooks.make.tapAsync(
-    //   'AlterManifestWebpackPlugin',
-    //   (compilation, callback) => {
-    //     console.log(compilation.hooks.faviconsWebpackPlugin);
-    //     console.log(compilation.hooks.faviconsWebpackPluginMake);
-    //     console.log(compilation.hooks.faviconsWebpackPluginCompile);
-    //     console.log(compilation.hooks.faviconsWebpackPluginEmit);
-    //     console.log(compilation.hooks.faviconsWebpackPluginHtmlPluginBefore);
-
-    //     console.log(compilation.hooks.FaviconsWebpackPlugin);
-    //     console.log(compilation.hooks.FaviconsWebpackPluginMake);
-    //     console.log(compilation.hooks.FaviconsWebpackPluginCompile);
-    //     console.log(compilation.hooks.FaviconsWebpackPluginEmit);
-    //     console.log(compilation.hooks.FaviconsWebpackPluginHtmlPluginBefore);
-    //     // compilation.hooks.webappWebpackPluginBeforeEmit.tapAsync(
-    //     //   'B',
-    //     //   (result, callback) => {
-    //     //     // The result of favicons library can be modified here
-    //     //     // and it will be returned to WebApp Plugin to be emitted.
-    //     //     // Add your custom functions below
-    //     //     console.log(result);
-    //     //     // Return the custom result
-    //     //     return callback(null, result);
-    //     //   }
-    //     // );
-    //     return callback();
-    //   }
-    // );
   }
 }
 module.exports = AlterManifestWebpackPlugin;
